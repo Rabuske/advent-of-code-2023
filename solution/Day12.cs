327a7c85604e4a24eaa77e64c7ca3bbcd1e4cc1f
Felipe Rabuske Costa <rabuske@outlook.com>
2023-12-16 19:02:41 -0300

    Day 12 🎄 - Finally

--- solution/Day12.cs
+++ solution/Day12.cs
@@ -1,8 +1,9 @@
-
 using System.Text.RegularExpressions;
 
 class Day12 : IDayCommand
 {
+  private static Dictionary<string, long> Memory = new();
+
   public string Execute()
   {
     // Parsing shenanigans
@@ -12,50 +13,101 @@ class Day12 : IDayCommand
     {
       var split = line.Split(" ");
       var springs = split[0];
-      var records = split[1].Split(',').Select(int.Parse).ToList();
+      var records = split[1].Split(',').Select(sequence => int.Parse(sequence)).ToArray();
       return (springs, records);
     });
 
-    var springsAndConditionRecordsPart02 = springsAndConditionRecordsPart01.Select(spRec => (
-      springs: string.Join("", Enumerable.Repeat(spRec.springs, 5)), 
-      records: Enumerable.Repeat(spRec.records, 5).SelectMany(i => i).ToList()
-    )); 
+    var springsAndConditionRecordsPart02 = springsAndConditionRecordsPart01.Select(spRec => {
+      var multipleSprings = string.Join("?", Enumerable.Repeat(spRec.springs, 5));
+      var multipleRecords = Enumerable.Repeat(spRec.records, 5).SelectMany(i => i).ToArray();
+      return (springs: multipleSprings, records: multipleRecords);
+    }).ToList();
 
-    var numberOfValidCombinationsPart01 = springsAndConditionRecordsPart01.Select(spRec => ProcessSprings(spRec.springs, spRec.records));
-    var numberOfValidCombinationsPart02 = springsAndConditionRecordsPart02.Select(spRec => ProcessSprings(spRec.springs, spRec.records));
+    var numberOfValidCombinationsPart01 = springsAndConditionRecordsPart01.Select(spRec =>
+      CalculateNumberOfAllowedConfigurations(Regex.Replace(spRec.springs, @"\.+", ".") + '.', spRec.records)).ToList();
+    var numberOfValidCombinationsPart02 = springsAndConditionRecordsPart02.Select(spRec =>
+      CalculateNumberOfAllowedConfigurations(Regex.Replace(spRec.springs, @"\.+", ".") + '.', spRec.records)).ToList();
+
 
     return $"""
-      The sum of all permutations is {1}
+      The sum of all permutations is {numberOfValidCombinationsPart01.Sum()}
       The sum of all permutations is when unfolded is {numberOfValidCombinationsPart02.Sum()}
       """;
   }
 
-  private int ProcessSprings(string springs, List<int> records)
+  private long CalculateNumberOfAllowedConfigurations(string springs, int[] groups)
   {
-    // First, generate all the possible combinations of spring
-    var springPermutations = new List<string>{ " " };
-    foreach(var spring in springs)
+    var stringRepresentation = springs + string.Join(",", groups);
+    if (Memory.ContainsKey(stringRepresentation))
     {
-      springPermutations = springPermutations.SelectMany(permutation => {
-        var newPermutations = new List<string>();
-        if(spring == '?')
-        {
-          newPermutations.Add(permutation + '#');
-          newPermutations.Add(permutation + '.');
-        }
-        else 
-        {
-          newPermutations.Add(permutation + spring);
-        }
-        return newPermutations;
-      }).ToList();
+      return Memory[stringRepresentation];
     }
-    
-    // Generate a Regex based on the records
-    string regexString = $@"^[.]*{string.Join("", records.Select(record => string.Join("", Enumerable.Repeat('#', record)) + @"[.]+"))}";
-    regexString = regexString[..^1] + "*$";
 
-    var regex = new Regex(regexString);
-    return springPermutations.Where(permutation => regex.IsMatch(permutation.Trim())).Count();
+    if (springs.Length == 0)
+    {
+      if (groups.Length == 0)
+      {
+        Memory.Add(stringRepresentation, 1);
+        return 1;
+      }
+      else
+      {
+        Memory.Add(stringRepresentation, 0);
+        return 0;
+      }
+    }
+
+    if (groups.Length == 0)
+    {
+      if (springs.All(s => s == '.' || s == '?'))
+      {
+        Memory.Add(stringRepresentation, 1);
+        return 1;
+      }
+      else
+      {
+        Memory.Add(stringRepresentation, 0);
+        return 0;
+      }
+    }
+
+    if (springs[0] == '.')
+    {
+      var result = CalculateNumberOfAllowedConfigurations(springs[1..], groups);
+      Memory.Add(stringRepresentation, result);
+      return result;
+    }
+
+    var numberOfSprings = groups[0];
+    if (springs[0] == '#')
+    {
+      if (numberOfSprings + 1 > springs.Length)
+      {
+        Memory.Add(stringRepresentation, 0);
+        return 0;
+      }
+      if (springs[0..numberOfSprings].All(s => s != '.') && springs[numberOfSprings] != '#')
+      {
+        var result = CalculateNumberOfAllowedConfigurations(springs[(numberOfSprings + 1)..], groups[1..]);
+        Memory.Add(stringRepresentation, result);
+        return result;
+      }
+      else
+      {
+        Memory.Add(stringRepresentation, 0);
+        return 0;
+      }
+    }
+
+    if (springs[0] == '?')
+    {
+      var result = CalculateNumberOfAllowedConfigurations('.' + springs[1..], groups) +
+                   CalculateNumberOfAllowedConfigurations('#' + springs[1..], groups);
+      Memory.Add(stringRepresentation, result);
+      return result;
+    }
+
+    Memory.Add(stringRepresentation, 0);
+    return 0;
   }
 }
